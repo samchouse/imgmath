650741643ed5848b1e2055beff61a9512eef0a3d
Samuel Corsi-House <chouse.samuel@gmail.com>
2023-08-15 11:29:57 -0400

    feat: it compiles

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,12 @@ fn main() {
 
     img.pixels_mut().for_each(|pixel| {
         let mut color = vec3(pixel[0] as f64, pixel[1] as f64, pixel[2] as f64);
-        // color *= mix(
-        //     &vec3(1.0, 1.0, 1.0),
-        //     &vec3(1.0, 1.0, 1.0),
-        //     // dot(
-        //     //     color,
-        //     //     vec3(0.2126, 0.7152, 0.0722)
-        //     //         / max(dot(color, vec3(0.2126, 0.7152, 0.0722)), 1e-5),
-        //     // ),
-        //     1.0,
-        // );
 
-        color = mix(&color, &color, 1.0);
+        let a = mat3(
+            color[0], color[1], color[2], color[0], color[1], color[2], color[0], color[1],
+            color[2],
+        ) * color_temp_to_rgb(4000.0);
+        color = mix(&color, &a, 1.0);
 
         let out = vec4(color[0], color[1], color[2], pixel[3] as f64);
 
@@ -27,7 +21,7 @@ fn main() {
     img.save("out.png").unwrap();
 }
 
-fn color_temp_to_rgb(temperature: f64) {
+fn color_temp_to_rgb(temperature: f64) -> TVec3<f64> {
     let m = mat3(
         0.0,
         -2902.1955373783176,
@@ -40,11 +34,16 @@ fn color_temp_to_rgb(temperature: f64) {
         1.8993753891711275,
     );
 
-    // nalgebra::Matrix1x3::<u64>::zeros() / nalgebra::Matrix3x1::<u64>::zeros();
-
     let a = clamp_scalar(temperature, 1000.0, 40000.0);
     let b = vec3(a, a, a);
-    // let c =
-    //     mat3(m[0], m[0], m[0], m[0], m[0], m[0], m[0], m[0], m[0]) / (b + vec3(m[1], m[1], m[1]));
-    // let d = c + vec3(m[2], m[2], m[2]);
+    let c = b + vec3(m[1], m[1], m[1]);
+    let d = vec3(m[0], m[0], m[0]).component_div(&c);
+    let e = d + vec3(m[2], m[2], m[2]);
+    let f = clamp_vec(&e, &vec3(0.0, 0.0, 0.0), &vec3(1.0, 1.0, 1.0));
+
+    mix(
+        &f,
+        &vec3(1.0, 1.0, 1.0),
+        smoothstep(1000.0, 0.0, temperature),
+    )
 }
